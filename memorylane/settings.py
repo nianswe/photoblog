e0429488e6ca80a64225f791795e6b365784da82
Niklas Andersson <nianswe@hotmail.com>
2024-05-21 20:43:00 +0000

    Troubleshute Heroku deployment4

--- memorylane/settings.py
+++ memorylane/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.localhost']
+ALLOWED_HOSTS = [,'.herokuapp.com', '127.0.0.1', '.localhost']
 
 
 # Application definition
