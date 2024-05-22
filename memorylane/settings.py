bc38e64c12632ffd7eefad51fd66cebb64547c73
Niklas Andersson <nianswe@hotmail.com>
2024-05-22 06:03:54 +0000

    Heroku deployment

--- memorylane/settings.py
+++ memorylane/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -32,8 +33,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = True
 
 DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = False
 
-ALLOWED_HOSTS = [, '.herokuapp.com', '127.0.0.1', '.localhost', '.memorylane-b6f09b53ccac.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.localhost', '.herokuapp.com']
 
 
 # Application definition
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
