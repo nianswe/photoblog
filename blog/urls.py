85b7d249793f052750d030e75bcf08f114c06912
Niklas Andersson <nianswe@hotmail.com>
2024-05-28 08:53:58 +0000

    Add alt name to images

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,6 @@ from . import views
 from django.urls import path
 import blog.views
 
-import authentication.views
-
 urlpatterns = [
     # path('', views.PostList.as_view(), name='post_list'),
     
